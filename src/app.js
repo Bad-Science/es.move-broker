b47455d27e647b15274c17e013b1aed2ba9c766c
Richie <richiejoeyoung@gmail.com>
2017-04-30 01:18:11 -0400

    adds expiremental server, ready for first testing

---
+++ src/app.js
@@ -0,0 +1,62 @@
+import SocketIO from 'socket.io';
+import express from 'express';
+import http from 'http';
+
+import Environment from './environment';
+
+let app = express();
+let server = http.Server(app);
+let io = new SocketIO(server);
+let port = process.env.PORT || 3000;
+let users = [];
+let sockets = {};
+
+class World {
+  constructor () {
+    this._environmentsByName = {};
+    this._environmentsById = {};
+  }
+
+  function registerEnvironment (name, connection) {
+    const env = new Environment(name, connection);
+    if (this._environmentsByName[name] === undefined) {
+      this._enironmentsByName[name] = {};
+    }
+    this._environmentsByName[name][env.id] = env;
+    this._environmentsById[env.id] = env;
+    return env;
+  }
+
+  function getAnyEnvironment (name) {
+    const allIds = Object.keys(this._environmentsByName[name]);
+    const id = Math.floor(Math.random * allIds.length);
+    return this._environmentsByName[name][id];
+  }
+
+  function getEnvironmentFor (locator) {
+    const [name, id] = locator.split(':');
+    if (id) {
+      return this._environmentsByName[name][id];
+    } else {
+      return getAnyEnvironment(name);
+    }
+  }
+}
+
+class Broker {
+
+}
+
+const broker = new Broker();
+const world = new World();
+
+io.on('connection', (socket) => {
+  const name = socket.handshake.query.name;
+  const environment = world.registerEnvironment(name, socket);
+  socket.emit('assignId', environment.id);
+
+  socket.on('moveAction', (locator, action) => {
+    nextEnvironment = world.getEnvironmentFor(action);
+    nextEnvironment.emit('receiveAction', action);
+  });
+});
---
+++ src/environment.js
@@ -0,0 +1,11 @@
+nextId = 1;
+
+class Environment {
+  constructor(name, connection) {
+    this.name = name;
+    this.id = nextId++;
+    this.connection = connection;
+  }
+}
+
+export default Environment;
